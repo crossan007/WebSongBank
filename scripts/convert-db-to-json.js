81ccf32e80a17e69e14e5c96c863126ccc217f2a
Charles Crossan <crossan007@gmail.com>
2019-04-02 20:07:21 -0400

    udpate database conversion script

--- scripts/convert-db-to-json.js
+++ scripts/convert-db-to-json.js
@@ -1,10 +1,20 @@
 const SqliteToJson = require('sqlite-to-json');
 const sqlite3 = require('sqlite3');
 const jsonfile = require('jsonfile')
+const libxmljs = require("libxmljs");
 
 const exporter = new SqliteToJson({
   client: new sqlite3.Database('./sqlite/songs.sqlite')
 });
+const getVerses = function(cdata) {
+    var XMLDoc = libxmljs.parseXml(cdata);
+    var verses = XMLDoc.find("//verse");
+    var verseTexts = verses.map(function(verse) {
+        return verse.text() ;
+    });
+       
+    return verseTexts;
+}
 exporter.all(function (err, all) {
     var fixed = all.songs.map(function(item) {
 
@@ -14,10 +24,12 @@ exporter.all(function (err, all) {
             Authors:  [],
             CCLINumber: item.ccli_number,
             SongKeys: [],
-            Links: []
+            Links: [],
+            Verses: getVerses(item.lyrics)
+
         }
     })
-    jsonfile.writeFile("./songs-dump.json", fixed, function (err) {
+    jsonfile.writeFile("./public/songs.json", fixed, function (err) {
         if (err) console.error(err)
     })
 });
